9637f5562161e4ce75c4235c8b23b2a32980b0f8
Soham Ashodiya <sohamashodiya@gmail.com>
2016-04-02 20:24:25 -0700

    Fixed publish.bat

--- publish.bat
+++ publish.bat
@@ -3,7 +3,7 @@ del /Q ..\soham1.github.io\*.map
 del /Q ..\soham1.github.io\*.js
 del /S /Q ..\soham1.github.io\assets
 del /Q ..\soham1.github.io\index.html
-copy dist\* ..\soham1.github.io
+xcopy dist ..\soham1.github.io /E
 cd ..\soham1.github.io
 git add --all
 git commit -m "New version"
